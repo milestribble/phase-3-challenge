b2c216d91e30b3fec0c21ee7555536bedf04ba25
Miles Tribble <timothy.tribble@gmail.com>
2017-10-02 10:22:24 -0700

    Instantiated express server on port 3000 and completed Route 1.

--- part-1/app.js
+++ part-1/app.js
@@ -2,11 +2,16 @@ import bodyParser from 'body-parser'
 import express from 'express'
 const app = express()
 
+app.disable('etag');
 
-app.use()
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get()
-
-app.post()
-
+app.get('/api/days/:day', (req, res) => {
+  let daysOfWeek = {monday: 1, tuesday:2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7}
+  let numericDay = daysOfWeek[req.params.day]
+  numericDay ? res.status(200).send(`${numericDay}`) : res.status(400).send(`'${req.params.day}' is not a valid day!`)
+})
+//
+// app.post()
+//
 app.listen(3000)
