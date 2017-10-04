084f221744eed4d1c3868f953d43d85ff9f9f2e8
Miles Tribble <timothy.tribble@gmail.com>
2017-10-03 17:33:53 -0700

    Verified test were breakable and passing when repaired.

--- part-2/database.js
+++ part-2/database.js
@@ -3,14 +3,14 @@ const { Client } = require('pg')
 const connectionString = process.env.NODE_ENV === 'test'
   ? 'postresql://localhost:5432/grocery_store_test'
   : 'postresql://localhost:5432/grocery_store'
-const client = new Client({connectionString})
-
 
 
 module.exports = {
   query : function (statement, params) {
     return new Promise(function (resolve, reject) {
-      client.connect()
+      let client = new Client({connectionString})
+      client.connect().catch(err=>console.log(`database "grocery_store" does not exist\n
+  Try running 'npm run db:reset'`))
       client.query(statement, params)
         .then(resolve)
         .catch(reject)
@@ -19,28 +19,32 @@ module.exports = {
   },
   productList : function (query) {
     return new Promise((resolve, reject) => {
-      if (!query) {console.error(`No query given`); process.exit(-1)}
-      this.query(`SELECT items.value as item, sections.value as section FROM items JOIN sections
+      if (!query) reject('No query given')
+      this.query(`SELECT items.value as "Product Name", sections.value as "Section" FROM items JOIN sections
         ON items.section_id = sections.self WHERE sections.value = $1`, [query])
-        .then(results => resolve(results.rows))
+        .then(results => typeof results.rows[0] === 'undefined'
+          ? reject('Section does not exist')
+          : resolve(results.rows))
     })
   },
   shopperOrders : function (query) {
     return new Promise((resolve, reject) => {
-      if (!query) {console.error(`No query given`); process.exit(-1)}
-      this.query(`SELECT order_items.order_id, SUM(items.price)
+      if (!query) reject('No query given')
+      this.query(`SELECT order_items.order_id as "Order ID", SUM(items.price) as "Total Cost"
                     FROM order_items
                   JOIN items
                     ON order_items.item_id = items.self
                   JOIN (SELECT self FROM orders WHERE shopper_id = $1) orders
                     ON orders.self = order_items.order_id
                   GROUP BY order_items.order_id`, [query])
-        .then(results => resolve(results.rows))
+        .then(results => typeof results.rows[0] === 'undefined'
+          ? reject('Shopper does not exist')
+          : resolve(results.rows))
     })
   },
   realShoppers : function (query) {
     return new Promise((resolve, reject) => {
-      this.query(`SELECT shoppers.first, COUNT(orders.self)
+      this.query(`SELECT shoppers.first as "Shopper Name", COUNT(orders.self) as "Number of Orders"
                     FROM shoppers
                   JOIN orders
                     ON shoppers.self = orders.shopper_id
--- part-2/store.js
+++ part-2/store.js
@@ -12,36 +12,39 @@ try {
 } catch (error) {
   if (error.message.includes('command') || error.message.includes('charAt')) {
     console.log(`${process.argv[2]} is not a valid sub-command.
-    Try product-list, shopper-orders, or real-shoppers.`);
+    Try product-list, shopper-orders, or real-shoppers.`)
     process.exit(1)
   } else {
-    console.log(error);
+    console.log(error)
   }
 }
 
 db[command](query)
   .then(tableData => {
-    let print = []
-    for (x in tableData[0]) {print.push([x, x.length])}
-    tableData.forEach((el) =>{
-      el[print[0][0]].length > print[0][1]
-        ? print[0][1] = el[print[0][0]].length : 0
-      el[print[1][0]].length > print[1][1]
-        ? print[1][1] = el[print[1][0]].length : 0
-      print.push( [ el[print[0][0]] , el[print[1][0]] ] )
-    })
-    while (print[0][0].length < print[0][1]) {print[0][0] += " "}
-    while (print[1][0].length < print[1][1]) {print[1][0] += " "}
-    for (let i = 2; i<print.length; i++) {
-      while (print[i][0].length < print[0][1]) {print[i][0] += " "}
-      while (print[i][1].length < print[1][1]) {print[i][1] += " "}
-     }
+    /* formatting */
+      let print = []
+      for (x in tableData[0]) {print.push([x, x.length])}
+      tableData.forEach((el) =>{
+        el[print[0][0]].length > print[0][1]
+          ? print[0][1] = el[print[0][0]].length : 0
+        el[print[1][0]].length > print[1][1]
+          ? print[1][1] = el[print[1][0]].length : 0
+        print.push( [ el[print[0][0]].toString() , el[print[1][0]].toString() ] )
+      })
+      while (print[0][0].length < print[0][1]) {print[0][0] += " "}
+      while (print[1][0].length < print[1][1]) {print[1][0] += " "}
+      for (let i = 2; i<print.length; i++) {
+        while (print[i][0].length < print[0][1]) {print[i][0] += " "}
+        while (print[i][1].length < print[1][1]) {print[i][1] += " "}
+       }
+    /* formatted */
     let linebreak = '|-'+'-'.repeat(print[0][1])+'-|-'+'-'.repeat(print[1][1])+'-|'
     console.log(linebreak);
-    console.log('| '+print[0][0]+' | '+print[1][0]+' |');
+      console.log('| '+print[0][0]+' | '+print[1][0]+' |');
     console.log(linebreak);
-    for (let i = 2; i<print.length; i++) {
-      console.log('| ' + print[i].join(' | ') + ' |');
-    }
+      for (let i = 2; i<print.length; i++) {
+        console.log('| ' + print[i].join(' | ') + ' |');
+      }
     console.log(linebreak);
   })
+  .catch(err=>console.log(err))
